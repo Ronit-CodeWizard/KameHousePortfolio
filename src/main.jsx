a8033161a291e21f3f3b4b8223af0c43717be29c
Maxxiiime <max2002guillon@gmail.com>
2024-04-06 15:28:46 +0200

    Add basics R3F templates

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+        <Canvas
+            camera={ {
+                fov: 45,
+                near: 0.2,
+                far: 200,
+                position: [3, 2, 6],
+            } }
+        >
+            <App />
+
+        </Canvas>
 )
