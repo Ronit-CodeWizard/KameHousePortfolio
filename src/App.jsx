e4f36492844a6677cedea9bbdc584aa41fb65912
Maxxiiime <max2002guillon@gmail.com>
2024-05-26 19:21:05 +0200

    Add Title text

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, useGLTF, Sky } from "@react-three/drei"
+import { useGLTF, Sky, Float, Text, OrbitControls } from "@react-three/drei"
 
 import Scene from './Components/Scene.jsx'
 import Ocean from "./Components/Ocean.jsx"
@@ -6,18 +6,31 @@ import Ocean from "./Components/Ocean.jsx"
 function App() {
   const { nodes } = useGLTF('./Model/House.glb')
 
+
   return (
     <>
-      <OrbitControls/>
 
+      <OrbitControls/>
       <directionalLight position={ [1, 2, 3] } intensity={ 2.5 }/>
 
       <Sky 
-        sunPosition={[1, 0.1, 1]}
-        distance={100000000}
-         
+        sunPosition={[1, 0.1, 1]}         
       />
-      
+    
+    <Float
+    rotationIntensity={0.7}
+    >
+        <Text
+          font="./fonts/font.ttf"
+          position-y={ 15 }
+          rotation-y={ 0.4166666667 * Math.PI }
+          curveRadius={ -50 }
+          fontSize={ 5 }
+        >
+          Maxime's Portfolio
+        </Text>
+        </Float>
+   
       <Scene/>
       <Ocean/>
 
