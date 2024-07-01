ad42666811407d4e061c55cb8ba47749d88376e6
Maxxiiime <max2002guillon@gmail.com>
2024-07-01 23:05:17 +0200

    Add CameraScroll component

--- src/Components/Scene.jsx
+++ src/Components/Scene.jsx
@@ -3,13 +3,17 @@ import { useGLTF } from '@react-three/drei'
 import House from './House'
 import Island from './Island'
 
+import CameraScroll from './CameraScroll'
+
 export default function Scene(props) {
 
     const { nodes } = useGLTF('./Model/House.glb')
+
     
     return <>
         <House nodes= { nodes }/>
         <Island nodes= { nodes } />
+        <CameraScroll nodes= { nodes }/>
     
     </>
 
