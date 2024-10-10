cebb08bc620c54cd55ff58428732565d982c6fa6
Maxxiiime <max2002guillon@gmail.com>
2024-10-10 16:22:36 +0200

    Add first shader in bedroom

---
+++ src/Components/House/ShaderTab.jsx
@@ -0,0 +1,37 @@
+import { useRef } from "react"
+import { useFrame } from "@react-three/fiber";
+
+import fragmentShader from '../../shaders/tab/fragment.glsl'
+import vertexShader from '../../shaders/tab/vertex.glsl'
+
+export default function ShaderTab(props) {
+
+    const planeRef = useRef()
+
+    const shaderMaterial = {
+      vertexShader: vertexShader,
+      fragmentShader: fragmentShader,
+      uniforms: {
+        uTime: { value: 0 }
+      }
+  
+    }
+  
+    useFrame(({ clock }) => {
+      planeRef.current.material.uniforms.uTime.value = clock.getElapsedTime();
+    });
+
+    
+    return <>
+         <mesh position={[0.1, 5.16, -2.4]} ref={ planeRef }>
+            <planeGeometry args={[1.13, 0.4]} />
+            <shaderMaterial 
+                attach="material" 
+                args={[shaderMaterial]}  
+            />
+        
+            </mesh>
+    
+    </>
+
+}
