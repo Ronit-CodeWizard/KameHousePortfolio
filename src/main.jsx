a041f2263d4f6e8982ff82e7c42042e289f8602d
Maxxiiime <max2002guillon@gmail.com>
2025-01-05 18:15:20 +0100

    Add some kind of responsove

--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,50 @@ import { Canvas } from '@react-three/fiber'
 import { Perf } from 'r3f-perf'
 import App from './App.jsx'
 
+const isMobile = () => {
+
+    console.log("Largeur = " + window.innerWidth)
+    console.log("Hauteur = " + window.innerHeight)
+    return ( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) );
+  }
+
+
+
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
-root.render(
-        <Canvas
-            camera={ {
-                fov: 45,
-                near: 0.1,
-                far: 200,
-                position: [52, 7, 12],
-            } }
-        >
-            <Perf position="top-left" />
-            <App />
+const canvaForMobile = () =>
+(
+    <Canvas
+        camera={{
+        fov: 100,
+        near: 0.1,
+        far: 200,
+        position: [52, 7, 12],
+        }}
         
+        >
+        <App/>
+    </Canvas>
 
-        </Canvas>
+)
+
+const canvaForPC = () =>
+(
+    <Canvas
+        camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [52, 7, 12],
+    }}
+    >
+        <App/>
+        <Perf position="top-left" />
+    </Canvas>
+    
+)
+
+root.render(
+
+    isMobile() ? canvaForMobile() : canvaForPC()
 )
