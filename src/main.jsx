9c873146f129c6c493e2a83f3bccdd26e0aa3263
Maxxiiime <max2002guillon@gmail.com>
2025-01-08 16:43:30 +0100

    Add strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import { Perf } from 'r3f-perf'
 import App from './App.jsx'
+import { StrictMode } from 'react'
 
 const isMobile = () => {
     return ( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) );
   }
-
-
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
@@ -16,17 +15,20 @@ const fovForMobile = 100
 const fovForPc = 45
 
 root.render(
-    <Canvas
-        camera={{
-        fov: isMobile() ? fovForMobile : fovForPc,
-        near: 0.1,
-        far: 200,
-        position: [52, 7, 12],
-    }}
-    >
-        <App/>
-        <Perf position="top-left" />
-    </Canvas>
+    <StrictMode>
+        <Canvas
+            camera={{
+            fov: isMobile() ? fovForMobile : fovForPc,
+            near: 0.1,
+            far: 200,
+            position: [52, 7, 12],
+        }}
+        >
+            
+            <App/>            
+            <Perf position="top-left" />
+        </Canvas>
+    </StrictMode>
 
     
 )
