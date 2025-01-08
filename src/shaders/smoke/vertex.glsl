4f07cefbfbea83fef727670d18f01593f1779ab6
Maxxiiime <max2002guillon@gmail.com>
2025-01-08 16:39:54 +0100

    Edit wind power

--- src/shaders/smoke/vertex.glsl
+++ src/shaders/smoke/vertex.glsl
@@ -24,7 +24,7 @@ void main()
     vec2 windOffset = vec2( texture(uPerlinTexture, vec2(0.25, uTime * 0.02)).r - 0.5,
                             texture(uPerlinTexture, vec2(0.75, uTime * 0.02)).r - 0.5);
 
-    windOffset *= pow(uv.y, 2.0) * 0.4;
+    windOffset *= pow(uv.y, 2.0) * 0.2;
     newPosition.xz += windOffset;
 
     // Final position
